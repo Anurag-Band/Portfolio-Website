5a9d9fb44c98498192dc76fde4b10307cc00d5e9
Anurag-Band <anuragband10@gmail.com>
2023-01-18 12:11:26 +0530

    prettier config added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Image from "next/image";
 import ProfilePic from "../../public/assets/profile-pic.png";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section>
+    <header>
       {/* Left Section */}
-      <div className="flex items-center space-x-5">
-        <Image 
-         src={ProfilePic}
-         alt="Profile Picture"
+      <Link
+        href={"/"}
+        className="flex items-center space-x-5"
+      >
+        <Image
+          src={ProfilePic}
+          alt="Profile Picture"
         />
         <p className="text-red-500">ANURAG BAND</p>
-      </div>
+      </Link>
       {/* Right Section */}
-      <div>
-
-      </div>
-    </section>
+      <nav></nav>
+    </header>
   );
 };
 
