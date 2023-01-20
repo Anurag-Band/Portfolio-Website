6bd98ad658555362dab6a784d3329e1206391d5d
Anurag-Band <anuragband10@gmail.com>
2023-01-20 08:52:01 +0530

    project header created

--- src/components/DivideLine.jsx
+++ src/components/DivideLine.jsx
@@ -2,7 +2,7 @@ import { RiMoreLine } from 'react-icons/ri'
 
 const DivideLine = () => {
   return (
-    <div className="relative flex items-center w-11/12 sm:pr-24 md:py-16">
+    <div className="relative flex items-center w-11/12 sm:pr-24 md:py-10">
       <div className="flex-grow border-t border-slate-400"></div>
       <span className="flex-shrink mx-4 text-slate-400">
         <RiMoreLine />
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,3 +1,4 @@
+import DivideLine from "@/components/DivideLine";
 import Head from "next/head";
 import React from "react";
 
@@ -11,9 +12,26 @@ const Projects = () => {
           content="Anurag Band | My Projects"
         />
       </Head>
-      <div>
-        <h1>Projects Section</h1>
-      </div>
+      <section
+        id="about"
+        className="min-h-[92vh] flex flex-col bg-slate-100 space-y-10 pt-24"
+      >
+        <div className="w-full h-[12vh] flex flex-col items-center justify-center space-y-4">
+          <h2 className="font-mainHeading font-medium text-4xl tracking-widest mt-24">
+            Projects I&#39;ve worked on
+          </h2>
+          <hr className="border border-b-4 border-b-teal-500 w-14" />
+          <p className="text-lg text-slate-500 font-medium">
+            These are some of the projects that I&#39;m most proud of. I&#39;ve
+            built them to learn new technologies, or to solve a problem that
+            I&#39;ve encountered.
+          </p>
+          <DivideLine />
+        </div>
+        <div className="flex flex-col container mx-auto px-5 xl:px-28 py-5 xl:py-14 w-full">
+
+        </div>
+      </section>
     </>
   );
 };
